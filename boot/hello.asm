7ba88c409fd7a20d5748ad01e3214f1286a67f22
jaymzee <jaymzee@users.noreply.github.com>
2020-12-02 16:33:15 -0500

    boot: hello world

--- boot/hello.asm
+++ boot/hello.asm
@@ -1,22 +1,31 @@
 	org 0x7c00
 _start:
-	mov ax, 0xB800
+	mov ax, 0xB800		; initialize es data segment for vga text
 	mov es, ax
-	xor si, si
-	xor di, di
-	mov cx, msglen
-write:
-	mov al, [si + msg]
-	mov [es: di], al
-	inc si
+clrscr	mov al, 0		; clrscr character
+	mov ah, 7		; clrscr attribute
+	mov di, 0		; index into screen memory
+	mov cx, 2000		; 80 x 25
+.L1	mov [es:di], al		; set character
 	inc di
+	mov [es:di], ah		; set attribute
 	inc di
-	loop write
-forever:
-	jmp forever
-msg:
+	loop .L1
+	mov si, greeting
+	mov di, 0		; index into screen memory
+	mov cx, greetingL
+.L2	mov al, [si]		; get next char from greeting
+	mov [es:di], al		; write to screen memory
+	inc si			; increment index for next char
+	add di, 2		; increment index for screen memory
+	loop .L2
+forever	jmp forever
+
+greeting:
 	db "hello, world!!"
-msglen	equ $ - msg
+greetingL equ $ - greeting
+padding:
 times 510-($-$$) db 0
+magic:
 	db 0x55, 0xaa
 
