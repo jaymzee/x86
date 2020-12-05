ca5440fe15088f5e94bfa108ef9c59f541a62603
jaymzee <jaymzee@users.noreply.github.com>
2020-12-04 22:47:47 -0500

    boot/bootloader: memory map was wrong

--- boot/bootloader/boot.asm
+++ boot/bootloader/boot.asm
@@ -1,17 +1,18 @@
+; floppy disk boot block
+
 	org 0x7c00
 _start:
 	mov ah, 2h	; read sectors from drive
-	mov al, 60	; sector count
+	mov al, 59	; sector count
 	mov ch, 0	; cylinder
 	mov dh, 0	; head
 	mov cl, 2	; sector
-	mov dl, 0	; select drive a (80h for 1st hd)
-	mov bx, 0x100
+	mov dl, 0	; drive a (use 80h for 1st HD)
+	mov bx, 500h	; start address of next boot loader
 	int 13h
-	jmp 100h
+	jmp 500h	; jump to 2nd level boot loader
 
 padding:
 times 510-($-$$) db 0
 magic:
 	db 0x55, 0xaa
-
--- boot/bootloader/boot2.asm
+++ boot/bootloader/boot2.asm
@@ -1,24 +1,15 @@
-	org 0x100
+	org 0x500
 _start:
-	mov ax, 0xB800		; initialize es data segment for vga text
-	mov es, ax
-clrscr	mov al, 0		; clrscr character
-	mov ah, 7		; clrscr attribute
-	mov di, 0		; index into screen memory
-	mov cx, 2000		; 80 x 25
-.L1	mov [es:di], al		; set character
-	inc di
-	mov [es:di], ah		; set attribute
-	inc di
-	loop .L1
 	mov si, greeting
-	mov di, 0		; index into screen memory
 	mov cx, greetingL
-.L2	movsb			; copy greeting to screen memory
-	inc di			; increment screen memory index again
-	loop .L2
+	mov ah, 0Eh		; teletype output
+	mov bx, 0		; page number
+.L1	mov al, [si]
+	int 10h
+	inc si
+	loop  .L1
 forever	jmp forever
 
 greeting:
-	db "hello, world!!"
+	db "hello, world!!", 13, 10, "Hi!"
 greetingL equ $ - greeting
