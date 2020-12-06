6ce9424dda2504010a17f3db6bded33887b57818
jaymzee <jaymzee@users.noreply.github.com>
2020-12-06 16:10:08 -0500

    boot/{vga;bootlaoder}: load address

--- boot/bootloader/stage0.asm
+++ boot/bootloader/stage0.asm
@@ -1,16 +1,18 @@
 ; floppy disk boot block
 
+LOADAD	equ 0x7e00
+
 	bits 16
 	global _start
 _start:
-	mov	ah, 2h	; read sectors from drive
-	mov	al, 59	; sector count
-	mov	ch, 0	; cylinder
-	mov	dh, 0	; head
-	mov	cl, 2	; sector
-	mov	dl, 0	; drive a (use 80h for 1st HD)
-	mov	bx, 500h	; start address of next boot loader
+	mov	ah, 2h		; read sectors from drive
+	mov	al, 32		; sector count
+	mov	ch, 0		; cylinder
+	mov	dh, 0		; head
+	mov	cl, 2		; sector
+	mov	dl, 0		; drive a (use 80h for 1st HD)
+	mov	bx, LOADAD	; start address of next boot loader
 	int	13h
-	jmp	500h	; jump to 2nd level boot loader
+	jmp	LOADAD		; jump to 2nd level boot loader
 	times 510-($-$$) db 0
-	db 0x55, 0xaa
+	db 0x55, 0xaa		; boot block signature
--- boot/vga/stage0.asm
+++ boot/vga/stage0.asm
@@ -1,18 +1,20 @@
-; floppy disk boot block
+; ----------------------------------------------------------------------
+; floppy disk stage 0 boot loader
+; assemble with nasm
+
+LOADAD	equ 0x7e00
+
 	bits 16
 	global _start
 _start:
-	mov ah, 2h	; read sectors from drive
-	mov al, 59	; sector count
-	mov ch, 0	; cylinder
-	mov dh, 0	; head
-	mov cl, 2	; sector
-	mov dl, 0	; drive a (use 80h for 1st HD)
-	mov bx, 500h	; start address of next boot loader
-	int 13h
-	jmp 0:500h	; jump to 2nd level boot loader
-
-padding:
-times 510-($-$$) db 0
-magic:
-	db 0x55, 0xaa
+	mov	ah, 2h		; read sectors from drive
+	mov	al, 59		; sector count
+	mov	ch, 0		; cylinder
+	mov	dh, 0		; head
+	mov	cl, 2		; sector
+	mov	dl, 0		; drive a (use 80h for 1st HD)
+	mov	bx, LOADAD	; start address of next boot loader
+	int	13h
+	jmp	LOADAD		; jump to 2nd level boot loader
+	times 510-($-$$) db 0
+	db 0x55, 0xaa		; boot block signature
