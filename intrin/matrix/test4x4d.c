7d032532eb884db79533d6150bbc000ca650cdfe
jaymzee <jaymzee@users.noreply.github.com>
2020-12-11 03:21:45 -0500

    intrin/matrix/test4x4d.c - seg fault on windows

--- intrin/matrix/test4x4d.c
+++ intrin/matrix/test4x4d.c
@@ -1,24 +1,30 @@
 #include "matrix4x4d.h"
 #include "print_avx.h"
+#include <stdio.h>
+
+__m256d a[4] = {
+    { 1,  2,  3,  4},
+    { 5,  6,  7,  8},
+    { 9,  3,  2,  1},
+    { 5,  4,  7,  6}
+};
+
+__m256d y, x = {1, 2, 3, 4};
 
 int main()
 {
-    __m256d a[4] = {
-        { 1,  2,  3,  4},
-        { 5,  6,  7,  8},
-        { 9,  3,  2,  1},
-        { 5,  4,  7,  6}
-    };
-    __m256d y, x = {1, 2, 3, 4};
+    int i;
 
-    for (int i = 0; i < 10000000; i++) {
-        y = m4x4v_rd(a, x);
+    for (i = 0; i < 10000; i++) {
+        y = m4x4v_cd(a, x);
     }
 
     y = m4x4v_cd(a, x);
     print_m256d(y);
     y = m4x4v_rd(a, x);
     print_m256d(y);
+
+    printf("success!");
 
     return 0;
 }
