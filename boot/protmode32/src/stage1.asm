0793889861718389520c3c223054e7f6c032d074
jaymzee <jaymzee@users.noreply.github.com>
2020-12-13 08:32:05 -0500

    boot/protmode32: cleanup

--- boot/protmode32/src/stage1.asm
+++ boot/protmode32/src/stage1.asm
@@ -13,35 +13,29 @@ VIDEOMEM    EQU 0x0b8000
 
 	global _start
 _start:
-	push	greeting
-	call	_bios_print
+	mov	si, greeting1
+	call	bios_print
 	jmp	enter_prot_mode
 
-; void bios_print(char *str)		; print to screen using BIOS INT 10h
-_bios_print:
-	push	bp
-	mov	bp, sp
+; print to screen using calls to BIOS INT 10h
+;   si  pointer to null terminated string
+;   ax  clobbered
+bios_print:
 	push	bx
-	push	si
-
 	cld
-	mov	si, [bp + 4]		; str
 	mov	bx, 0x000f		; page number (and color in gfx mode)
 	mov	ah, 0Eh			; teletype output
 .L1	lodsb
 	test	al, al
-	je	.done
+	je	.break
 	int	10h
 	jmp	.L1
-
-.done	pop	si
-	pop	bx
-	leave
-	retn	2
+.break	pop	bx
+	ret
 
 
 enter_prot_mode:
-	call	a20_on		; Enable A20 gate (uses Fast method as proof of concept)
+	call	a20_on		; Enable A20 gate
 	cli			; disable interrupts
 	lgdt	[gdtr]		; Load gdt
 	mov	eax, cr0
@@ -49,7 +43,9 @@ enter_prot_mode:
 	mov	cr0, eax	; Set protected mode flag
 	jmp	0x08:start_32
 
-; Enable a20 (fast method). This may not work on all hardware
+; Enable A20
+;   uses fast method as proof of concept and may not work on all hardware
+;   ax  clobbered
 a20_on:
 	cli			; disable interrupts
 	in	al, 0x92	; Read System Control Port A
@@ -59,17 +55,15 @@ a20_on:
 	and	al, 0xfe	; Always write a zero to bit 0 to avoid
 				;     a fast reset into real mode
 	out	0x92, al        ; Enable a20
-.skip:
-	sti			; reenable interrupts
+.skip	sti			; reenable interrupts
 	ret
 
 
 ; Code that will run in 32-bit protected mode
-; Align code to 4 byte boundary. code_32bit label is
-; relative to the origin point 100h
 
 	bits 32
 
+	section .code
 	align 4
 start_32:
 	cld			; Direction flag forward
@@ -84,31 +78,29 @@ start_32:
 
 	; print success message to display
 	mov	ah, 0x57	; Attribute white on magenta
-	mov	esi, pmode_msg	; ESI = address of string to print
+	mov	esi, greeting2	; ESI = address of string to print
 	mov	edi, VIDEOMEM	; EDI = base address of video memory
 	call	write_screen
 
 	mov	ebp, 0		; terminate chain of frame pointers
-	call	Main		; not expected for Main to return
-
-	cli			; disable interrupts
-.forever:
-	hlt			; Halt CPU with infinite loop
-	jmp	.forever
+	call	Main		; it's not expected for Main to ever return
+				; but just in case:
+	cli			;   disable interrupts
+.halt	hlt			;   Halt CPU with infinite loop
+	jmp	.halt
 
 ; display text and attribute at a location on the screen
-; edi - null terminated string to display
-; esi - address in video memory to begin writing string to
-; ah  - character attribute
+;   edi - null terminated string to display
+;   esi - screen memory address to write the string to
+;    ah - screen attribute to use for each character
+;   all other registers are preserved
 write_screen:
 	push	ecx
 	xor	ecx, ecx	; ECX = 0 current video offset
-	jmp	.loopentry
-.L1:
-	mov	[edi+ecx*2], ax	; Copy attr and character to display
+	jmp	.L1_ent
+.L1	mov	[edi+ecx*2], ax	; Copy attr and character to display
 	inc	ecx		; Next word position
-.loopentry:
-	mov	al, [esi+ecx]	; Get next character to print
+.L1_ent mov	al, [esi+ecx]	; Get next character to print
 	test	al, al
 	jnz	.L1		; If it's not NUL continue
 	pop	ecx
@@ -116,9 +108,9 @@ write_screen:
 
 
 	section .data
-greeting:
+greeting1:
 	db `Entering Protected Mode\r\n`, 0
-pmode_msg:
+greeting2:
 	db "Protected Mode entered successfully - console on serial 0", 0
 
 	align 8
