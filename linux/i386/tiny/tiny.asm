00048346cd0369f85cc32264bb067cc7be418943
jaymzee <jaymzee@users.noreply.github.com>
2021-07-21 09:56:45 +0000

    update tiny example

--- linux/i386/tiny/tiny.asm
+++ linux/i386/tiny/tiny.asm
@@ -1,8 +1,21 @@
-	BITS 32
-	GLOBAL _start
-	SECTION .text
+STDOUT	equ	1
+WRITE	equ	4
+EXIT	equ	1
 
+	default	rel
+	section .text
+
+	global _start
 _start:
-	mov	eax, 1
-	mov	ebx, 42
+	mov	eax, WRITE
+	mov	ebx, STDOUT
+	mov	ecx, mesg
+	mov	edx, mesglen
 	int	0x80
+
+	mov	eax, EXIT
+	mov	ebx, 42		; exit code
+	int	0x80
+
+mesg	db "Hello, World!", 10
+mesglen equ $ - mesg
